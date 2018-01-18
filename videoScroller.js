dd7bf92cc85a5a5f3c5e76ab94290a39de1cfcce
Scott Fischer <sfischerdesign@gmail.com>
2018-01-17 22:58:57 -0500

    added smart-autoplay functionality to video backgrounds, fixed safari bugs

---
+++ videoScroller.js
@@ -0,0 +1,34 @@
+function isElementInViewport(el) {
+
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
+
+var vid = document.getElementById("playTrigger");
+
+var playing = 0;
+$(window).on('scroll', function() {
+    if (isElementInViewport($("#videoContainer")) && (playing == 0)) {
+        $('#playTriggerTop')[0].pause();
+        $('#playTrigger')[0].play();
+        playing = 1;
+
+
+    } else if (!isElementInViewport($("#videoContainer")) && (playing == 1)) {
+        $('#playTrigger')[0].pause();
+        $('#playTriggerTop')[0].play();
+        playing = 0;
+    }
+});
